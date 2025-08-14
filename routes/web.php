74c0d414660218e7764044478ecebe8e0bcd9a04
Hamidreza <h.barnousi@gmail.com>
2025-08-14 22:32:11 +0330

    required routes for questions, answers, tags, voting, ...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\VoteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +14,23 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::resource('questions', QuestionController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+    Route::resource('questions.answers', AnswerController::class)->shallow()->only(['store', 'update', 'destroy']);
+
+    Route::post('/votes/{type}/{id}', [VoteController::class, 'store'])
+        ->middleware('throttle:10,1')
+        ->name('votes.store');
+
+    Route::delete('/votes/{type}/{id}', [VoteController::class, 'destroy'])
+        ->middleware('throttle:10,1')
+        ->name('votes.destroy');
+});
+
+Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
+Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
+Route::get('/questions/tagged/{tag:name}', [QuestionController::class, 'tagged'])->name('questions.tagged');
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
