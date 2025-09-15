7c6a55cae057ca922fe958d5485aa994a55fb762
Hamidreza <h.barnousi@gmail.com>
2025-09-15 18:50:29 +0330

    vote resource improvement

--- app/Http/Resources/VoteResource.php
+++ app/Http/Resources/VoteResource.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class VoteResource extends JsonResource
 {
+    protected function getVotableData()
+    {
+        return match ($this->votable_type) {
+            'question' => QuestionResource::make($this->whenLoaded('votable')),
+            'answer' => AnswerResource::make($this->whenLoaded('votable')),
+            default => null
+        };
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -16,6 +25,9 @@ class VoteResource extends JsonResource
     {
         return [
             'is_upvote' => $this->is_upvote,
+
+            'votable_type' => $this->votable_type,
+            'votable' => $this->whenLoaded('votable', fn() => $this->getVotableData()),
         ];
     }
 }
