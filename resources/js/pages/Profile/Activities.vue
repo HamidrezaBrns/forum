39fcdca1395275b3daf8436b515852f0630b4061
Hamidreza <h.barnousi@gmail.com>
2025-12-26 00:54:08 +0330

    a layout for more flexibility created

--- resources/js/pages/Profile/Activities.vue
+++ resources/js/pages/Profile/Activities.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import ActivityItem from '@/components/ActivityItem.vue';
-import Container from '@/components/Container.vue';
 import DetailedPagination from '@/components/DetailedPagination.vue';
-import ProfileInfoCard from '@/components/ProfileInfoCard.vue';
-import AppLayout from '@/layouts/AppLayout.vue';
+import ProfileLayout from '@/layouts/ProfileLayout.vue';
 import { formatNumber } from '@/utilities/number';
 import { Head } from '@inertiajs/vue3';
 import 'remixicon/fonts/remixicon.css';
@@ -14,28 +12,18 @@ defineProps(['user', 'activities']);
 <template>
     <Head :title="$t('Profile')" />
 
-    <AppLayout>
-        <Container>
-            <div class="grid grid-cols-12 gap-y-8 lg:gap-x-10">
-                <aside class="col-span-full lg:col-span-4">
-                    <ProfileInfoCard :user="user" class="sticky top-25" />
-                </aside>
+    <ProfileLayout :user="user">
+        <div class="mx-auto max-w-[90%]">
+            <h2 class="mb-4 text-3xl font-medium">
+                {{ $t('Activities (:total)', { total: formatNumber(activities.meta.total) }) }}
+            </h2>
 
-                <div class="col-span-full lg:col-span-8">
-                    <div class="mx-auto max-w-[90%]">
-                        <h2 class="mb-4 text-3xl font-medium">
-                            {{ $t(':total Activities', { total: formatNumber(activities.meta.total) }) }}
-                        </h2>
-
-                        <div class="grid grid-cols-1 gap-20 border-s border-gray-300 dark:border-gray-700">
-                            <ActivityItem v-for="activity in activities.data" :key="activity.id" :activity="activity" />
-                        </div>
-
-                        <!-- Pagination -->
-                        <DetailedPagination :meta="activities.meta" class="mt-10" />
-                    </div>
-                </div>
+            <div class="grid grid-cols-1 gap-20 border-s border-gray-300 dark:border-gray-700">
+                <ActivityItem v-for="activity in activities.data" :key="activity.id" :activity="activity" />
             </div>
-        </Container>
-    </AppLayout>
+
+            <!-- Pagination -->
+            <DetailedPagination :meta="activities.meta" class="mt-10" />
+        </div>
+    </ProfileLayout>
 </template>
